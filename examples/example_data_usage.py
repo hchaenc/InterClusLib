249dc55a4a652d2350b68c036e24caa9ef219360
Jiashu CHEN <scyjc9@exmail.nottingham.ac.uk>
2025-03-11 12:58:25 +0000

    Jiashu - remove Interval SOM

--- examples/example_data_usage.py
+++ examples/example_data_usage.py
@@ -9,7 +9,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # 添加 `interClusLib` 的上级目录到 Python 路径
 sys.path.append(os.path.join(current_dir, ".."))
 import interClusLib
-from interClusLib.data_structure import IntervalData
+from interClusLib import IntervalData
 
 print(interClusLib.__file__)
 
