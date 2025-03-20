6fa9d666aa31a55328d76daf79423a92380d5fae
Jiashu CHEN <scyjc9@exmail.nottingham.ac.uk>
2025-03-20 13:29:50 +0000

    Jiashu - clustering __init__ update

---
+++ interClusLib/clustering/__init__.py
@@ -0,0 +1,22 @@
+"""
+Interval Clustering Algorithms
+
+This package provides various clustering algorithms designed specifically for
+interval-valued data. Interval data is structured as arrays with shape (n_samples, n_dims, 2),
+where the last dimension represents the lower and upper bounds of each interval.
+
+Available algorithms:
+- IntervalKMeans: K-means clustering adapted for interval data
+- IntervalFuzzyCMeans: Fuzzy C-means clustering for interval data
+- IntervalAgglomerativeClustering: Agglomerative hierarchical clustering for interval data
+"""
+
+from .IntervalKMeans import IntervalKMeans
+from .IntervalFuzzyCMeans import IntervalFuzzyCMeans
+from .IntervalAgglomerativeClustering import IntervalAgglomerativeClustering
+
+__all__ = [
+    'IntervalKMeans',
+    'IntervalFuzzyCMeans',
+    'IntervalAgglomerativeClustering',
+]
